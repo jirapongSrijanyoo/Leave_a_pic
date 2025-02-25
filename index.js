203281f8c50faeff6775d3acc5c5f580d8a8ae59
loma_0531 <92037217+jirapongSrijanyoo@users.noreply.github.com>
2025-02-25 16:30:46 +0700

    fix port to 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const routes = require('./routes');
 
 const app = express();
-const PORT = 5500;
+const PORT = 3000;
 
 // กำหนดให้ Express serve ไฟล์จากโฟลเดอร์ uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
